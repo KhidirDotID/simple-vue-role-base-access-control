a2168d814fe851d42c000c3f0a8636df61aad71e
KhidirDotID <khidirdotid@gmail.com>
2020-11-20 06:37:09 +0700

    add router, store, and plugins

--- src/main.js
+++ src/main.js
@@ -1,8 +1,14 @@
-import Vue from 'vue'
-import App from './App.vue'
+import Vue from "vue";
+import "./plugins/axios";
+import "./plugins/helper";
+import App from "./App.vue";
+import router from "./router";
+import store from "./store";
 
-Vue.config.productionTip = false
+Vue.config.productionTip = false;
 
 new Vue({
-  render: h => h(App),
-}).$mount('#app')
+  router,
+  store,
+  render: (h) => h(App),
+}).$mount("#app");
